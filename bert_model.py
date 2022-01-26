fix bugs: add attention mask

bert_model.py
@@ -58,7 +58,7 @@ class BertForSequenceClassification(BertPreTrainedModel):
 
         self.bert = BertModel(config)
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
-        if model_type == "chunking":
+        if model_type == "with_chunking":
             self.extra_bert = BertEncoder(config,num_syntax_layers)
         self.pooler = BertPooler(config) 
         self.classifier = nn.Linear(config.hidden_size, config.num_labels)
@@ -95,7 +95,7 @@ class BertForSequenceClassification(BertPreTrainedModel):
 
         if self.model_type == "no_syntax":
             pooled_output = outputs[1]
-        elif self.model_type == "chunking":
+        elif self.model_type == "with_chunking":
             device = next(self.parameters()).device
             hidden_states = outputs[0]
             bs, L, _ = hidden_states.shape

loader.py
@@ -37,7 +37,9 @@ class DataLoader(object):
                     data = data["wp_ids"]
                 else:
                     data = list(zip(data["wp_ids"],data["const_end_markers"]))
-        
+       
+        if args.debug:
+            data = data[:args.num_debug] 
         # shuffle the data for training set
         if not inference and not eval:
             np.random.seed(args.seed)
@@ -61,18 +63,18 @@ class DataLoader(object):
 
         if self.model_type == "no_syntax":
             if self.inference:
-                batch_wp_ids = self._padding(self.device,batch)
+                batch_wp_ids, batch_masks = self._padding(batch)
             else:
                 batch_wp_ids, batch_labels = list(zip(*batch))
-                batch_wp_ids = self._padding(self.device,batch_wp_ids)
-            encoding = {"input_ids":batch_wp_ids}
+                batch_wp_ids, batch_masks = self._padding(batch_wp_ids)
+            encoding = {"input_ids":batch_wp_ids,"attention_mask":batch_masks}
         elif self.model_type == "with_chunking":
             if self.inference:
                 batch_wp_ids, batch_const_end_markers = list(zip(*batch))
             else:
                 batch_wp_ids, batch_const_end_markers, batch_labels = list(zip(*batch))
-            batch_wp_ids = self._padding(self.device,batch_wp_ids)
-            encoding = {"input_ids":batch_wp_ids,"wp2const":batch_const_end_markers}
+            batch_wp_ids, batch_masks = self._padding(batch_wp_ids)
+            encoding = {"input_ids":batch_wp_ids,"attention_mask":batch_masks,"wp2const":batch_const_end_markers}
         else:
             if self.inference:
                 batch_wp_ids, batch_subtree_masks = list(zip(*batch))
@@ -94,7 +96,8 @@ class DataLoader(object):
         max_len = max(map(len,wp_ids))
         wp_ids = torch.Tensor([line + (max_len-len(line)) * [0] for line in wp_ids]).int().to(self.device)
         if len(masks) == 0:
-            return wp_ids
+            padded_masks = torch.Tensor([len(line) * [1] + (max_len-len(line)) * [0] for line in wp_ids]).int().to(self.device)
+            return wp_ids, padded_masks
         padded_masks = []
         for m in masks:
             tmp_mask = np.zeros((max_len,max_len))

modeling.py
@@ -122,9 +122,12 @@ def train(args, train_dataloader,dev_dataloader,model,ensemble_id):
     optimizer = Adam(model.parameters(),lr=args.learning_rate)
 
     if args.grid_search:
-        best_model_dir = os.path.join(f"{args.bert_variant}_{args.model_type}",f"bs_{args.batch_size}_lr_{args.learning_rate}",f"ensemble_{ensemble_id}")
+        best_model_dir = os.path.join(f"{args.bert_variant}_{args.model_type}",f"bs_{args.batch_size}_lr_{args.learning_rate}_num_extra_layers_{args.num_syntax_layers}",f"ensemble_{ensemble_id}")
     else:
-        best_model_dir = os.path.join(f"{args.bert_variant}_{args.model_type}",f"ensemble_{ensemble_id}"))
+        best_model_dir = os.path.join(f"{args.bert_variant}_{args.model_type}",f"ensemble_{ensemble_id}")
+    output_dir = os.path.join(args.finetuned_model_path,best_model_dir)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
     # Train
     logger.info("***** Running training *****")
@@ -178,9 +181,6 @@ def train(args, train_dataloader,dev_dataloader,model,ensemble_id):
             best_epoch = epoch
             
             # save model
-            output_dir = os.path.join(args.finetuned_model_path,best_model_dir)
-            if not os.path.exists(output_dir):
-                os.makedirs(output_dir)
             model.save_pretrained(output_dir)
             torch.save(args, os.path.join(output_dir, 'training_args.bin'))
             logger.info("new best model! saved.")
@@ -189,10 +189,7 @@ def train(args, train_dataloader,dev_dataloader,model,ensemble_id):
             max_score = dev_score
             best_epoch = epoch
 
-            # save model
-            output_dir = os.path.join(args.finetuned_model_path,best_model_dir)
-            if not os.path.exists(output_dir):
-                os.makedirs(output_dir)
+            # save model 
             model.save_pretrained(output_dir)
             torch.save(args,os.path.join(output_dir,"training_args.bin"))
             logger.info("new best model! saved.")